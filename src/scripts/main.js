09d04fa8cbeddffe2f807242e3a27bdeba44c5a7
Krzysztof Kalinowski <krzykalinowski@gmail.com>
2024-04-29 17:10:59 +0200

    added const decalrations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
-console.log("test");
+const container = document.querySelector(".app-room");
+const seats = document.querySelectorAll(
+  ".seats-row__seat:not(.seats-row__seat--occupied)"
+);
+const count = document.querySelector(".app-summary__places");
+const price = document.querySelector(".app-summary__price");
+const movieSelect = document.getElementById("movie-select");
+
+const ticketPrice = parseInt(movieSelect.value);
+
+container.addEventListener("click", (e) => {
+  if (e.target.classList.contains("seats-row__seat")) {
+    console.log("seat clicked");
+  }
+});
